177007db0eac48eb223b72c2f865eebf8f0bbba0
Tomas Dvorsky <tomas.dvorsky@gmail.com>
2024-04-05 01:19:30 +0200

    pts-control init

---
+++ src/main/java/cz/pts/ptscontrol/PtsControlApplication.java
@@ -0,0 +1,13 @@
+package cz.pts.ptscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtsControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtsControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/pts/ptscontrol/PtsControlApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.pts.ptscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtsControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
